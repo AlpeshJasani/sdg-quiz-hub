5148fcc6d31ce3dcf852a15ab4f6e9e8bbe2744c
Alpesh Jasani <alpeshjasani777@gmail.com>
2024-10-05 21:22:27 +0530

    fixing 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import SDGQuizPage from "./pages/SDGQuizPage";
-import Footer from "./components/Footer";
 
 function App() {
     return (
